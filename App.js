4fc5c66359af00e05721c16d53d631003564a890
Firoj <mymindhungry@gmail.com>
2022-01-20 15:13:02 +0545

    map

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    Dimensions,
+    TouchableOpacity,
+    Text,
+    View,
+    Touchable
+} from "react-native";
+import Mapview, { PROVIDER_GOOGLE } from "react-native-maps";
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [dark, setDark] = useState(false)
+    return ( <
+        View style = { styles.container } >
+        <
+        Mapview style = { style.map }
+        provider = { PROVIDER_GOOGLE } >
+        <
+        TouchableOpacity onPress = {
+            () => setDark(!dark) }
+        style = {
+            {
+                backgroundColor: "#FFF",
+                height: 30,
+                borderRadius: 15,
+                width: 30,
+                alignItems: "center",
+                justifyContent: "center",
+                position: "absolute",
+                marginTop: 60,
+                alignSelf: 'flex-end',
+                right: 20,
+
+
+            }
+        } >
+        <
+        FontAwesome name = "adjust"
+        size = { 30 }
+        /> <
+        /TouchableOpacity> <
+        /Mapview> <
+        /View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height
+    }
 });
