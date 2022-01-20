99f86f0f4559951af0fd0ee94eca731889cac053
Firoj <mymindhungry@gmail.com>
2022-01-20 15:46:33 +0545

    map

--- App.js
+++ App.js
@@ -1,64 +1,277 @@
-// import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import {
-    StyleSheet,
-    Dimensions,
-    TouchableOpacity,
-    Text,
-    View,
-    Touchable
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
 } from "react-native";
-import Mapview, { PROVIDER_GOOGLE } from "react-native-maps";
-import FontAwesome from '@expo/vector-icons/FontAwesome'
-
-
-
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function App() {
-    const [dark, setDark] = useState(false)
-    return ( <
-        View style = { styles.container } >
-        <
-        Mapview style = { style.map }
-        provider = { PROVIDER_GOOGLE } >
-        <
-        TouchableOpacity onPress = {
-            () => setDark(!dark) }
-        style = {
-            {
-                backgroundColor: "#FFF",
-                height: 30,
-                borderRadius: 15,
-                width: 30,
-                alignItems: "center",
-                justifyContent: "center",
-                position: "absolute",
-                marginTop: 60,
-                alignSelf: 'flex-end',
-                right: 20,
-
-
-            }
-        } >
-        <
-        FontAwesome name = "adjust"
-        size = { 30 }
-        /> <
-        /TouchableOpacity> <
-        /Mapview> <
-        /View>
-    );
+  const [dark, setDark] = useState(false);
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={dark ? mapStyle : null}
+      >
+        <TouchableOpacity
+          onPress={() => setDark(!dark)}
+          style={{
+            backgroundColor: "#FFF",
+            height: 30,
+            borderRadius: 15,
+            width: 30,
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            marginTop: 60,
+            alignSelf: "flex-end",
+            right: 20,
+          }}
+        >
+          <FontAwesome name="adjust" size={30} />
+        </TouchableOpacity>
+      </MapView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    map: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
+
+const mapStyle = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#212121",
+      },
+    ],
+  },
+  {
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        saturation: -5,
+      },
+      {
+        lightness: -5,
+      },
+    ],
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#212121",
+      },
+    ],
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9E9E9E",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#BDBDBD",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#181818",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1B1B1B",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#2C2C2C",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#8A8A8A",
+      },
+    ],
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#373737",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#3C3C3C",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway.controlled_access",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#4E4E4E",
+      },
+    ],
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#000000",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#3D3D3D",
+      },
+    ],
+  },
+];
